1ed8e432e090334152984edfb1690c3ab4810212
Rob Ranf <robranf@gmail.com>
2021-02-13 16:52:46 -0800

    refactored to move connection to initDB.js

---
+++ initDB.js
@@ -0,0 +1,26 @@
+const { connect, connection } = require("mongoose");
+const { config } = require("dotenv");
+
+module.exports = () => {
+	config();
+
+	// const dbName = process.env.DB_NAME;
+	// const user = process.env.DB_USER;
+	// const pass = process.env.DB_PASS;
+	const uri = `mongodb+srv://waifu-service:equEcVmDn9mtJ8R@cluster0.mdczd.mongodb.net/waifuchanDB?retryWrites=true&w=majority`;
+
+	connect(uri, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useFindAndModify: false,
+		useCreateIndex: true,
+	})
+		.then(() => {
+			console.log("Connection established with MongoDB");
+		})
+		.catch((error) => console.error(error.message));
+
+	connection.on("connected", () => {
+		console.log("Mongoose connected to DB Cluster");
+	});
+};
--- server.js
+++ server.js
@@ -1,19 +1,35 @@
-require("dotenv").config;
 const express = require("express");
 const bodyParser = require("body-parser");
+require("./initDB")();
 const mongoose = require("mongoose");
 // const _ = require("lodash");
+const Schema = mongoose.Schema;
 
 const app = express();
-const dbPassword = process.env.DB_PASS;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect(
-	`mongodb+srv://waifu-service:${dbPassword}@cluster0.mdczd.mongodb.net/waifus?retryWrites=true&w=majority`,
-	{
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-		useFindAndModify: false,
-	}
-);
+const waifuSchemaShort = new Schema({
+	waifuId: Number,
+	fullNameEn: String,
+	animeMainSeries: String,
+	profileImageURL: String,
+	deleted: Boolean,
+});
+
+const Waifu = mongoose.model("Waifu", waifuSchemaShort);
+
+app.route("/waifus").get((req, res) => {
+	Waifu.find({}, (err, foundWaifus) => {
+		if (err) {
+			res.send(err);
+		} else {
+			res.send(foundWaifus);
+		}
+	});
+});
+
+app.listen(process.env.PORT || 3000, () => {
+	let port = process.env.PORT || 3000;
+	console.log("Waifu-chan API service started on port " + port);
+});
